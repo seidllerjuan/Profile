dc3e3351bc8e1553237f6de8cf0faad064b073f0
oNovella <omar.novella@iest.edu.mx>
2026-02-13 19:32:27 -0600

    Add: Implementacion de eventos en contexto de imagenes y botones, asi como refinamiento de vistas y adicion de portada en la interfaz

--- app/src/main/java/com/example/myapplication/Profile.kt
+++ app/src/main/java/com/example/myapplication/Profile.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Notifications
